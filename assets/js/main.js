046eeb1ccc56729468bd3db7d04b1097d9100c31
Bernardo Araujo <bernardoaraujons@gmail.com>
2024-05-27 22:38:39 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,20 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', () =>{
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
@@ -22,56 +22,45 @@ if(navClose){
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-// Function to toggle div visibility based on theme
-const toggleDivs = () => {
-    if (getCurrentTheme() === 'light') {
-      lightModeDiv.style.display = 'flex';
-      darkModeDiv.style.display = 'none';
-    } else {
-      lightModeDiv.style.display = 'none';
-      darkModeDiv.style.display = 'flex';
-    }
-  };
-
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-function scrollHeader(){
+function scrollHeader() {
     const header = document.getElementById('header')
     // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+    if (this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== QUESTIONS ACCORDION ===============*/
 const accordionItems = document.querySelectorAll('.questions__item')
 
-accordionItems.forEach((item) =>{
+accordionItems.forEach((item) => {
     const accordionHeader = item.querySelector('.questions__header')
 
-    accordionHeader.addEventListener('click', () =>{
+    accordionHeader.addEventListener('click', () => {
         const openItem = document.querySelector('.accordion-open')
 
         toggleItem(item)
 
-        if(openItem && openItem!== item){
+        if (openItem && openItem !== item) {
             toggleItem(openItem)
         }
     })
 })
 
-const toggleItem = (item) =>{
+const toggleItem = (item) => {
     const accordionContent = item.querySelector('.questions__content')
 
-    if(item.classList.contains('accordion-open')){
+    if (item.classList.contains('accordion-open')) {
         accordionContent.removeAttribute('style')
         item.classList.remove('accordion-open')
-    }else{
+    } else {
         accordionContent.style.height = accordionContent.scrollHeight + 'px'
         item.classList.add('accordion-open')
     }
@@ -81,87 +70,82 @@ const toggleItem = (item) =>{
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
+function scrollActive() {
     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
-              sectionId = current.getAttribute('id')
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
+        } else {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
 }
 window.addEventListener('scroll', scrollActive)
 
-/*=============== SHOW SCROLL UP ===============*/ 
-function scrollUp(){
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
     const scrollUp = document.getElementById('scroll-up');
     // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+    if (this.scrollY >= 400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
-/*=============== DARK LIGHT THEME ===============*/ 
+/*=============== DARK LIGHT THEME ===============*/
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'ri-sun-line'
-// Divs to toggle
-const lightModeDiv = document.querySelector('.imgOdsLightMode');
-const darkModeDiv = document.querySelector('.imgOdsDarkMode');
 
 // Images to toggle classes
-const lightModeImg = document.querySelector('.imgOdsLightMode img');
-const darkModeImg = document.querySelector('.imgOdsDarkMode img');
+const lightModeImg = 'imgLight';
+const darkModeImg = 'imgDark';
 
 // Previously selected theme (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme');
 const selectedIcon = localStorage.getItem('selected-icon');
 
+const divOds = document.querySelector('.odsImg');
+const divOdsImg = document.querySelector('.odsImg img');
+const srcDark = "assets/img/ods15-white.png"
+const srcLight = "assets/img/ods15.png"
+
 // We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line';
 
-// Function to toggle div visibility based on theme
-const toggleDivsAndImages = () => {
-  if (getCurrentTheme() === 'light') {
-    lightModeDiv.style.display = 'flex';
-    darkModeDiv.style.display = 'none';
-    lightModeImg.classList.add('imgLight');
-    lightModeImg.classList.remove('imgDark');
-    darkModeImg.classList.add('imgDark');
-    darkModeImg.classList.remove('imgLight');
-  } else {
-    lightModeDiv.style.display = 'none';
-    darkModeDiv.style.display = 'flex';
-    lightModeImg.classList.add('imgLight');
-    lightModeImg.classList.remove('imgDark');
-    darkModeImg.classList.add('imgDark');
-    darkModeImg.classList.remove('imgLight');
-  }
-};
-
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
-  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme);
-  toggleDivsAndImages(); // Ensure divs and images are correctly toggled on load
+const getImgTheme = () => {
+    if (getCurrentTheme() === 'dark') {
+        divOdsImg.classList.add(darkModeImg)
+        divOdsImg.classList.remove(lightModeImg)
+        divOdsImg.src = srcDark
+        console.log(darkModeImg)
+    } else {
+        divOdsImg.classList.remove(darkModeImg)
+        divOdsImg.classList.add(lightModeImg)
+        divOdsImg.src = srcLight
+        console.log(lightModeImg)
+    }
 }
 
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme);
+    // Ensure divs and images are correctly toggled on load
+}
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
-  // Add or remove the dark / icon theme
-  document.body.classList.toggle(darkTheme);
-  themeButton.classList.toggle(iconTheme);
-  // We save the theme and the current icon that the user chose
-  localStorage.setItem('selected-theme', getCurrentTheme());
-  localStorage.setItem('selected-icon', getCurrentIcon());
-  toggleDivsAndImages(); // Toggle divs and images based on new theme
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+    getImgTheme();// Toggle divs and images based on new theme
 });
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
@@ -174,8 +158,10 @@ const sr = ScrollReveal({
 })
 
 sr.reveal(`.home__data`)
-sr.reveal(`.home__img`, {delay: 500})
-sr.reveal(`.home__social`, {delay: 600})
-sr.reveal(`.about__img, .contact__box`,{origin: 'left'})
-sr.reveal(`.about__data, .contact__form`,{origin: 'right'})
-sr.reveal(`.odsImg, .questions__title, .questions__objective, .steps__card, .product__card, .questions__group, .footer`,{interval: 100})
+sr.reveal(`.home__img`, { delay: 500 })
+sr.reveal(`.home__social`, { delay: 600 })
+sr.reveal(`.about__img, .contact__box`, { origin: 'left' })
+sr.reveal(`.about__data, .contact__form`, { origin: 'right' })
+sr.reveal(`.odsImg, .questions__title, .questions__objective, .steps__card, .product__card, .questions__group, .footer`, { interval: 100 })
+
+document.body.addEventListener('onload', getImgTheme())
